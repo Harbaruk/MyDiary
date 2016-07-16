e0d03cda07a048f9a6e5f371bf3b002cd2787fe6
SenjorBogdan <bohdan213111@gmail.com>
2016-07-16 19:03:55 +0300

    UserEvents
    створений клас UserEvents

---
+++ MyDiary/MyDiary/UserEvents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDiary
+{
+    public class UserEvents
+    {
+        private DateTime TimeOfCreation { get; set; }
+        public string Text { get; set; }
+        public DayMark Mark { get; set; }
+
+        public UserEvents() { }
+        // Всі дані
+        public UserEvents(string text, DayMark mark)
+        {
+            Text = text;
+            TimeOfCreation = DateTime.Now;
+            Mark = mark;
+        }
+        //Текст і час створення
+        public UserEvents(string text)
+        {
+            Text = text;
+            TimeOfCreation = DateTime.Now;
+            Mark = DayMark.Unknown;
+        }
+    }
+}
