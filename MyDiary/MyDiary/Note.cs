e2c4b4fa9a71b2528cae5151c1d1e916a6ff221b
SenjorBogdan <bohdan213111@gmail.com>
2016-07-16 16:42:24 +0300

    Note (Constructors)
    Добавлено конструктори до Note

--- MyDiary/MyDiary/Note.cs
+++ MyDiary/MyDiary/Note.cs
@@ -17,5 +17,31 @@ namespace MyDiary
         private DateTime TimeOfCreation { get; set; }
         public string Text { get; set; }
         public DayMark Mark { get; set; }
+
+        public Note() { }
+        // Всі дані
+        public Note(string text,DateTime timeOfCreation, DayMark mark)
+        {
+            Text = text; 
+            TimeOfCreation = timeOfCreation;
+            Mark = mark;
+        }
+        //Текст і час створення
+        public Note(string text, DateTime timeOfCreation)
+        {
+            Text = text;
+            TimeOfCreation = timeOfCreation;
+        }
+        //Текст і відмітку
+        public Note(string text, DayMark mark)
+        {
+            Text = text; 
+            Mark = mark;
+        }
+        // Текст
+        public Note(string text)
+        {
+            Text = text;
+        }
     }
 }
