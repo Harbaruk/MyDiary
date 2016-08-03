b1833805458a2ec3e0c4847b1b6c3fdaa741f155
SenjorBogdan <bohdan213111@gmail.com>
2016-08-03 23:17:03 +0300

    MainForm Add Buttons

--- MyDiary/MyDiary/MainForm.Designer.cs
+++ MyDiary/MyDiary/MainForm.Designer.cs
@@ -31,7 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.NotesButton = new System.Windows.Forms.Button();
             this.EventsButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.OptionsButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // NotesButton
@@ -41,7 +43,7 @@
             this.NotesButton.ForeColor = System.Drawing.Color.Blue;
             this.NotesButton.Location = new System.Drawing.Point(12, 12);
             this.NotesButton.Name = "NotesButton";
-            this.NotesButton.Size = new System.Drawing.Size(77, 32);
+            this.NotesButton.Size = new System.Drawing.Size(88, 32);
             this.NotesButton.TabIndex = 0;
             this.NotesButton.Text = "Notes";
             this.NotesButton.UseVisualStyleBackColor = false;
@@ -52,21 +54,42 @@
             this.EventsButton.BackColor = System.Drawing.Color.Transparent;
             this.EventsButton.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.EventsButton.ForeColor = System.Drawing.Color.Blue;
-            this.EventsButton.Location = new System.Drawing.Point(12, 50);
+            this.EventsButton.Location = new System.Drawing.Point(12, 73);
             this.EventsButton.Name = "EventsButton";
-            this.EventsButton.Size = new System.Drawing.Size(77, 32);
+            this.EventsButton.Size = new System.Drawing.Size(88, 32);
             this.EventsButton.TabIndex = 1;
             this.EventsButton.Text = "Events";
             this.EventsButton.UseVisualStyleBackColor = false;
             // 
-            // label1
+            // monthCalendar1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 114);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.monthCalendar1.Location = new System.Drawing.Point(279, 18);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 3;
+            // 
+            // OptionsButton
+            // 
+            this.OptionsButton.BackColor = System.Drawing.Color.Transparent;
+            this.OptionsButton.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OptionsButton.ForeColor = System.Drawing.Color.Blue;
+            this.OptionsButton.Location = new System.Drawing.Point(12, 137);
+            this.OptionsButton.Name = "OptionsButton";
+            this.OptionsButton.Size = new System.Drawing.Size(88, 32);
+            this.OptionsButton.TabIndex = 4;
+            this.OptionsButton.Text = "Options";
+            this.OptionsButton.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(311, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // MainForm
             // 
@@ -75,14 +98,15 @@
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(584, 319);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(450, 324);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.OptionsButton);
+            this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.EventsButton);
             this.Controls.Add(this.NotesButton);
             this.Name = "MainForm";
             this.Text = "MyDiary";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -90,7 +114,9 @@
 
         private System.Windows.Forms.Button NotesButton;
         private System.Windows.Forms.Button EventsButton;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button OptionsButton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
